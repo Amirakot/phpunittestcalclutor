d548f2df0cd5ea303144f94aaa53a3e56df89974
amirakot <amirakotb16270@yahoo.com>
2024-02-13 09:54:52 +0200

    first

---
+++ Calcutor.php
@@ -0,0 +1,9 @@
+<?php
+class Calcutor{
+public function add($x,$y){
+return $x+$y;
+}
+public function multiply($x,$y){
+return $x*$y;
+}
+}
---
+++ tests/CalclutorTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require './Calcutor.php';
+class CalclutorTest extends TestCase
+{
+    public function test_add(){
+        $calc=new Calcutor();
+       return $this->assertEquals($calc->add(2,2),4);
+    }
+      public function test_multiply(){
+        $calc=new Calcutor();
+       return $this->assertSame($calc->multiply(2,5),10);
+    }
+    public function test_sub(){
+        $calc=new Calcutor();
+       return $this->assertSame($calc->multiply(2,5),10);
+    }
+}
